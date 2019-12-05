34828f196951ac4e157cf5a4a7674833239ce693
Pavel Bondar <ivizey@gmail.com>
2019-12-05 20:33:26 +0200

    Added item view

---
+++ Geekhub iOS_HW_7/AddItemView.swift
@@ -0,0 +1,27 @@
+//
+//  AddItemView.swift
+//  Geekhub iOS_HW_7
+//
+//  Created by Pavel Bondar on 05.12.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class AddItemView: UIViewController {
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBAction func groupSelection(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            print("0")
+        case 1:
+            print("1")
+        case 2:
+            print("2")
+        default:
+            print("Acrion")
+        }
+    }
+}
