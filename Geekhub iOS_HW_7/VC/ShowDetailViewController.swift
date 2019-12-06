2e36964d37235b6f577963a71265c7411783a3db
Pavel Bondar <ivizey@gmail.com>
2019-12-06 20:53:27 +0200

    Move to vc

---
+++ Geekhub iOS_HW_7/VC/ShowDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowDetailViewController.swift
+//  Geekhub iOS_HW_7
+//
+//  Created by Pavel Bondar on 06.12.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ShowDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
